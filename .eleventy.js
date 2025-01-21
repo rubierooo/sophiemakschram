c3a09cf3bda767268739a27d0922148fea1e51a7
Rubie <13jibber@gmail.com>
2025-01-21 09:45:21 +0000

    Update .eleventy.js

--- .eleventy.js
+++ .eleventy.js
@@ -26,11 +26,13 @@ module.exports = function (eleventyConfig) {
 
   // Copy Static Files to /_Site
   eleventyConfig.addPassthroughCopy({
-    "./src/admin/config.yml": "./admin/config.yml": ""./src/admin/cms.js":
+    "./src/admin/config.yml": "./admin/config.yml",
     "./node_modules/alpinejs/dist/cdn.min.js": "./static/js/alpine.js",
     "./node_modules/prismjs/themes/prism-tomorrow.css":
       "./static/css/prism-tomorrow.css",
   });
+
+  eleventyConfig.addPassthroughCopy("./src/admin/cms.js");
 
   // Copy Image Folder to /_site
   eleventyConfig.addPassthroughCopy("./src/static/img");
